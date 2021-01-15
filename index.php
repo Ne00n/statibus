1051bfb6200ffa38179f8e82f2a117c866486746
Neo <neo@tiefkuehler-madness.me>
2021-01-15 19:29:41 +0100

    set viewport

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $percentages = $statibus->gimmahDowntimePercentaaages($uptime);
 <head>
   <meta charset="utf-8">
   <title><?php echo _title; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/code.css">
 </head>
 
