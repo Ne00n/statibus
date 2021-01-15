c54aadecc9385b1015356406a33ee18de7c4fe84
Neo <neo@tiefkuehler-madness.me>
2021-01-15 03:52:59 +0100

    update cron

---
+++ class/cron.php
@@ -0,0 +1,88 @@
+<?php
+
+class cron {
+
+  private $rqlite;
+  private $uptime;
+
+  public function __construct($rqliteIP,$rqlitePort) {
+    $this->rqlite = new rqlite($rqliteIP,$rqlitePort);
+  }
+
+  public function run() {
+    $services = $this->rqlite->select('SELECT * FROM services');
+    $this->uptime = $this->rqlite->select('SELECT * FROM uptime');
+    foreach ($services['values'] as $service) {
+      print("Checking ".$service[4]."\n");
+      if ($service[3] == "ping") {
+        exec("ping -c 3 " . $service[4], $output, $result);
+        if ($result == 0) { $status = 1; } else { $status = 0; }
+        $this->updateStatus($service[0],$status,$service[2]);
+      }
+    }
+  }
+
+  private function updateStatus($id,$current,$oldState) {
+    $this->updateUptime($id);
+    if ($current == 0 && $oldState == 1) {
+      print($id." went offline\n");
+      $this->rqlite->insert('INSERT INTO outages (serviceID,status,timestamp) VALUES("'.$id.'",0,'.time().')');
+      $this->rqlite->update('UPDATE services SET status = 0 WHERE id="'.$id.'"');
+    } elseif ($current == 1 && $oldState == 0) {
+      print($id." went is back online\n");
+      $this->rqlite->insert('INSERT INTO outages (serviceID,status,timestamp) VALUES("'.$id.'",1,'.time().')');
+      $this->rqlite->update('UPDATE services SET status = 1 WHERE id="'.$id.'"');
+    } else {
+      print($id." no change\n");
+    }
+  }
+
+  private function updateUptime($id) {
+    $data = tools::getUptimeFromService($id,$this->uptime);
+    if ($data == False) {
+      $this->rqlite->insert('INSERT INTO uptime(serviceID,detailed,oneDay,sevenDays,fourteenDays,thirtyDays,ninetyDays) VALUES("'.$id.'","W10=","100.00","100.00","100.00","100.00","100.00")');
+    }
+  }
+
+  private function calcWindow($outages,$window=1) {
+    $line = time() - (86400 * $window); $last = 0; $total = 0;
+    for ($i = 0; $i <= count($outages['values']) -1; $i++) {
+      $row = $outages['values'][$i];
+      if ($row[3] > $line) {
+        if ($row[2] == 1 && $last != 0) { $total = $total + ($row[3] - $last); }
+        if ($row[2] == 0) { $last = $row[3]; } else { $last = 0; }
+      }
+    }
+    if ($last != 0) { $total = $total + (time() - $last); }
+    return round($total / 60);
+  }
+
+  private function calcUptime($outages) {
+    $response = array();
+    $response[1] = 100 -bcmul( ($this->calcWindow($outages,1) / (1440 * 1) ),100,2);
+    $response[7] = 100 -bcmul( ($this->calcWindow($outages,7) / (1440 * 7) ),100,2);
+    $response[14] = 100 - bcmul( ($this->calcWindow($outages,14) / (1440 * 14) ),100,2);
+    $response[30] = 100 - bcmul( ($this->calcWindow($outages,30) / (1440 * 30) ),100,2);
+    $response[90] = 100 - bcmul( ($this->calcWindow($outages,90) / (1440 * 90) ),100,2);
+    return $response;
+  }
+
+  public function uptime() {
+    $uptime = $this->rqlite->select('SELECT * FROM uptime');
+    foreach ($uptime['values'] as $row) {
+      $outages = $this->rqlite->select('SELECT * FROM outages WHERE serviceID = '.$row[0].' ');
+      if (!isset($outages['values'])) {
+        $response = $this->rqlite->update('UPDATE uptime SET oneDay = 100.00,sevenDays = 100.00,fourteenDays = 100.00,thirtyDays = 100.00,ninetyDays = 100.00 WHERE serviceID = '.$row[0].' ');
+      } else {
+        $data = $this->calcUptime($outages);
+        $detailed = json_decode(base64_decode($row[1])); $current = date("d.m");
+        $detailed[$current] = $data[1];
+        $detailed = base64_encode(json_encode($detailed));
+        $response = $this->rqlite->update('UPDATE uptime SET detailed = "'.$detailed.'", oneDay = '.$data[1].',sevenDays = '.$data[7].',fourteenDays = '.$data[14].',thirtyDays = '.$data[30].',ninetyDays = '.$data[90].' WHERE serviceID = '.$row[0].' ');
+      }
+    }
+  }
+
+}
+
+?>
