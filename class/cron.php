3e0a3c54df0acae3e2977d5295b11093626dcd24
Neo <neo@tiefkuehler-madness.me>
2021-01-16 14:52:00 +0100

    PRECISION IS KING

--- class/cron.php
+++ class/cron.php
@@ -75,11 +75,11 @@ class cron {
 
   private function calcUptime($outages) {
     $response = array();
-    $response[1] = 100 -bcmul( ($this->calcWindow($outages,1) / (1440 * 1) ),100,2);
-    $response[7] = 100 -bcmul( ($this->calcWindow($outages,7) / (1440 * 7) ),100,2);
-    $response[14] = 100 - bcmul( ($this->calcWindow($outages,14) / (1440 * 14) ),100,2);
-    $response[30] = 100 - bcmul( ($this->calcWindow($outages,30) / (1440 * 30) ),100,2);
-    $response[90] = 100 - bcmul( ($this->calcWindow($outages,90) / (1440 * 90) ),100,2);
+    $response[1] = 100 -bcmul( ($this->calcWindow($outages,1) / bcmul(1440 * 1,2) ),100,2);
+    $response[7] = 100 -bcmul( ($this->calcWindow($outages,7) / bcmul(1440 * 7,2) ),100,2);
+    $response[14] = 100 - bcmul( ($this->calcWindow($outages,14) / bcmul(1440 * 14,2) ),100,2);
+    $response[30] = 100 - bcmul( ($this->calcWindow($outages,30) /  bcmul(1440 * 30,2) ),100,2);
+    $response[90] = 100 - bcmul( ($this->calcWindow($outages,90) / bcmul(1440 * 90,2) ),100,2);
     return $response;
   }
 
