231f4f983f8f72c75fd84abe8d1985d2a9a0b2ed
Neo <neo@tiefkuehler-madness.me>
2021-01-19 13:34:41 +0100

    added info messages on init

--- class/rqlite.php
+++ class/rqlite.php
@@ -49,14 +49,19 @@ class rqlite {
 
   public function init() {
     $result = $this->insert("CREATE TABLE services (id INTEGER NOT NULL PRIMARY KEY,groupID INTEGER NOT NULL,name TEXT NOT NULL,status INTEGER NOT NULL, method TEXT NOT NULL,target TEXT NOT NULL,timeout INTEGER NULL,httpcodes TEXT NULL,keyword TEXT NULL,lastrun INTEGER NULL,FOREIGN KEY(groupID) REFERENCES groups(id))");
+    print(tools::checkResult($result));
     if (!$result) { return $result; }
     $result = $this->insert("CREATE TABLE outages (id INTEGER NOT NULL PRIMARY KEY,serviceID INTEGER NOT NULL,status INTEGER NOT NULL, timestamp INTEGER NOT NULL,flag INTEGER NULL,FOREIGN KEY(serviceID) REFERENCES services(id) ON DELETE CASCADE)");
+    print(tools::checkResult($result));
     if (!$result) { return $result; }
     $result = $this->insert("CREATE TABLE uptime (serviceID INTEGER NOT NULL PRIMARY KEY, detailed TEXT NOT NULL,oneDay DECIMAL(7,4) NOT NULL, sevenDays DECIMAL(7,4) NOT NULL, fourteenDays DECIMAL(7,4) NOT NULL, thirtyDays DECIMAL(7,4) NOT NULL, ninetyDays DECIMAL(7,4) NOT NULL, FOREIGN KEY(serviceID) REFERENCES services(id) ON DELETE CASCADE)");
+    print(tools::checkResult($result));
     if (!$result) { return $result; }
     $result = $this->insert("CREATE TABLE groups (id INTEGER NOT NULL PRIMARY KEY,name TEXT NOT NULL)");
+    print(tools::checkResult($result));
     if (!$result) { return $result; }
     $result = $this->insert("PRAGMA foreign_keys = ON");
+    print(tools::checkResult($result));
     if (!$result) { return $result; }
     return $result;
   }
