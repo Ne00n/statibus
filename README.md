d634642987ccd4e76824056e3f48cfd62523682a
Neo <neo@tiefkuehler-madness.me>
2021-01-20 14:54:27 +0100

    use name instead of id + list returns json

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ php cli.php init
 4. You can add the first service by running:
 ```
 php cli.php group add Servers
-php cli.php service add 1 Server ping 8.8.8.8
+php cli.php service add Servers Server ping 8.8.8.8
 ```
 5. Enable the cronjobs, see => configs/cron|uptime.example<br />
 Run cron every 60s, uptime is for generating the uptime percentages, every 5 minutes is fine
@@ -43,16 +43,16 @@ SQL Migrations: https://github.com/Ne00n/statibus/tree/main/migrations
 ```
 Examples:
 ```
-php cli.php service add 1 Server ping 8.8.8.8
-php cli.php service add 1 Service port 8.8.8.8:80 2
-php cli.php service add 1 Website http https://website.com 2 200
-php cli.php service add 1 Website http https://website.com 2 400,404
-php cli.php service add 1 Keyword http https://keyword.com 2 200 clusterfuck
+php cli.php service add Servers Server ping 8.8.8.8
+php cli.php service add Servers Service port 8.8.8.8:80 2
+php cli.php service add Servers Website http https://website.com 2 200
+php cli.php service add Servers Website http https://website.com 2 400,404
+php cli.php service add Servers Keyword http https://keyword.com 2 200 clusterfuck
 ```
 - other
 ```
 php cli.php service list
-php cli.php service delete <id>
+php cli.php service delete <name>
 ```
 **group**<br />
 - add
@@ -65,5 +65,5 @@ php cli.php group list
 ```
 - delete
 ```
-php cli.php group delete 1
+php cli.php group delete test
 ```
--- cli.php
+++ cli.php
@@ -21,20 +21,22 @@ if (count($argv) == 1) {
     $rqlite->init();
   } elseif ($argv[1] == 'service') {
     if ($argv[2] == 'add') {
+      $response = $rqlite->select('SELECT id FROM groups WHERE name="'.$argv[3].'"',True);
+      if (isset($response['rows'])) { $groupID = $response['rows'][0]['id']; } else { echo "Error: Group not found\n"; die(); }
       if (!isset($argv[7])) { $argv[7] = 3; }
       if (!isset($argv[8])) { $argv[8] = 200; }
       if (!isset($argv[9])) { $argv[9] = null; }
-      $response = $rqlite->insert('INSERT INTO services(groupID,name,status,method,target,timeout,httpcodes,keyword) VALUES("'.$argv[3].'","'.$argv[4].'",1,"'.$argv[5].'","'.$argv[6].'","'.$argv[7].'","'.$argv[8].'","'.$argv[9].'")');
+      $response = $rqlite->insert('INSERT INTO services(groupID,name,status,method,target,timeout,httpcodes,keyword) VALUES("'.$groupID.'","'.$argv[4].'",1,"'.$argv[5].'","'.$argv[6].'","'.$argv[7].'","'.$argv[8].'","'.$argv[9].'")');
       if (isset($response['error']) && $response != False) { print("Error: ".($response != False ? $response['error'] : "rqlite not reachable.")."\n"); die(); }
       $response = $rqlite->insert('INSERT INTO uptime(serviceID,detailed,oneDay,sevenDays,fourteenDays,thirtyDays,ninetyDays) VALUES("'.$response["content"]["results"][0]["last_insert_id"].'","W10=","100.00","100.00","100.00","100.00","100.00")');
       print(tools::checkResult($response));
     } elseif ($argv[2] == 'list') {
       $response = $rqlite->select('SELECT * FROM services',True);
-      if (empty($response)) { echo "No services added.\n"; die(); }
+      if (empty($response)) { echo json_encode(array('error' => 'No services added.'),JSON_PRETTY_PRINT)."\n"; die(); }
       tools::checkRow($response);
-      foreach ($response['rows'] as $row) { print_r($row); }
+      echo json_encode($response['rows'],JSON_PRETTY_PRINT)."\n";
     } elseif ($argv[2] == 'delete') {
-      $response = $rqlite->delete('DELETE FROM services WHERE id='.$argv[3]);
+      $response = $rqlite->delete('DELETE FROM services WHERE name="'.$argv[3].'"');
       print(tools::checkResult($response));
     }
   } elseif ($argv[1] == 'group') {
@@ -43,11 +45,11 @@ if (count($argv) == 1) {
       print(tools::checkResult($response));
     } elseif ($argv[2] == 'list') {
       $response = $rqlite->select('SELECT * FROM groups',True);
-      if (empty($response)) { echo "No groups added.\n"; die(); }
+      if (empty($response)) { echo json_encode(array('error' => 'No groups added.'),JSON_PRETTY_PRINT)."\n"; die(); }
       tools::checkRow($response);
-      foreach ($response['rows'] as $row) { print_r($row); }
+      echo json_encode($response['rows'],JSON_PRETTY_PRINT)."\n";
     } elseif ($argv[2] == 'delete') {
-      $response = $rqlite->delete('DELETE FROM groups WHERE id='.$argv[3]);
+      $response = $rqlite->delete('DELETE FROM groups WHERE name="'.$argv[3].'"');
       print(tools::checkResult($response));
     }
   }
