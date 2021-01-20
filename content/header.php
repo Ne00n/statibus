5488d2eacb81131c0a7cecc4c75d41e38b190d5c
Neo <neo@tiefkuehler-madness.me>
2021-01-20 17:59:43 +0100

    update css

--- content/header.php
+++ content/header.php
@@ -5,6 +5,5 @@
   <meta charset="utf-8">
   <title><?php echo _title; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link async defer rel="stylesheet" href="content/css/style.css?v=2">
-  <link async defer rel="stylesheet" href="content/css/font.css?v=2">
+  <link rel="stylesheet" href="content/css/style.css?v=2">
 </head>
