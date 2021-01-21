b9684507e01672db47c3470134ce67d3c010a48b
Neo <neo@tiefkuehler-madness.me>
2021-01-21 03:19:54 +0100

    moah tests

---
+++ content/tests/statibusTest.php
@@ -0,0 +1,23 @@
+<?php
+
+#Load config
+include_once 'configs/config.example.php';
+
+use PHPUnit\Framework\TestCase;
+
+class rqliteTest extends TestCase {
+  private $statibus;
+
+  public function setUp(): void {
+    $this->statibus = new statibus(_rqliteIP,_rqlitePort);
+  }
+
+  public function testAddGroup(): void {
+    $this->assertTrue($this->statibus->groupAdd(array(3 => 'Servers')));
+  }
+
+  public function testDeleteGroup(): void {
+    $this->assertTrue($this->statibus->groupDelete(array(3 => 'Servers')));
+  }
+
+}
