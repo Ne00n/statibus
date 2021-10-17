5b2416e856bcd0ee52f97abbb8ff87f9eb3e93b3
Neoon <neo@tiefkuehler-madness.me>
2021-10-17 22:40:11 +0200

    cli update

--- cli.php
+++ cli.php
@@ -12,38 +12,46 @@ spl_autoload_register('dat_loader');
 $statibus = new statibus(_rqliteIP,_rqlitePort);
 
 if (count($argv) == 1) {
-  print("service add <group> <name> <method> <target> <timeout> <httpcode(s)> <keyword>\n");
-  print("group add <name>\n");
-  print("remote add <name> <url>\n");
-  print("service/group/remote delete <name>\n");
-  print("service/group/remote list\n");
+  print("service, group, remote\n");
 } else {
 
   if ($argv[1] == "init") {
     $statibus->sql()->init();
   } elseif ($argv[1] == 'service') {
-    if ($argv[2] == 'add') {
+    if (isset($argv[2]) && $argv[2] == 'add') {
       $statibus->serviceAdd($argv);
-    } elseif ($argv[2] == 'list') {
+    } elseif (isset($argv[2]) && $argv[2] == 'list') {
       $statibus->serviceList();
-    } elseif ($argv[2] == 'delete') {
+    } elseif (isset($argv[2]) && $argv[2] == 'delete') {
       $statibus->serviceDelete($argv);
+    } else {
+      print("service add <group> <name> <method> <target> <timeout> <httpcode(s)> <keyword>\n");
+      print("service list\n");
+      print("service delete <name>\n");
     }
   } elseif ($argv[1] == 'group') {
-    if ($argv[2] == 'add') {
+    if (isset($argv[2]) && $argv[2] == 'add') {
       $statibus->groupAdd($argv);
-    } elseif ($argv[2] == 'list') {
+    } elseif (isset($argv[2]) && $argv[2] == 'list') {
       $statibus->groupList();
-    } elseif ($argv[2] == 'delete') {
+    } elseif (isset($argv[2]) && $argv[2] == 'delete') {
       $statibus->groupDelete($argv);
+    } else {
+      print("group add <name>\n");
+      print("group list\n");
+      print("group delete <name>\n");
     }
   } elseif ($argv[1] == 'remote') {
-    if ($argv[2] == 'add') {
+    if (isset($argv[2]) && $argv[2] == 'add') {
       $statibus->remoteAdd($argv);
-    } elseif ($argv[2] == 'list') {
+    } elseif (isset($argv[2]) && $argv[2] == 'list') {
       $statibus->remoteList();
-    } elseif ($argv[2] == 'delete') {
+    } elseif (isset($argv[2]) && $argv[2] == 'delete') {
       $statibus->remoteDelete($argv);
+    } else {
+      print("remote add <name> <url>\n");
+      print("remote list\n");
+      print("remote delete <name>\n");
     }
   }
 
