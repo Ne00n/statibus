af783d02ac0e73b38b515c211d46979b4dc78602
Neoon <neo@tiefkuehler-madness.me>
2021-10-17 15:05:54 +0200

    header update

--- content/header.php
+++ content/header.php
@@ -5,6 +5,6 @@
   <meta charset="utf-8">
   <title><?php echo _title; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="preload" href="content/fonts/yusei-magic-v2-latin-regular.woff2" as="font" type="font/woff2">
+  <link rel="preload" href="content/fonts/yusei-magic-v2-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
   <link rel="stylesheet" href="content/css/style.css?v=3">
 </head>
