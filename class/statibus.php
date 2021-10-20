ea953ec6efef95e2e0c39c39ad75daadf14c9236
Neoon <neo@tiefkuehler-madness.me>
2021-10-20 15:16:09 +0200

    darkgreen 99.8% instead of less than 100%

--- class/statibus.php
+++ class/statibus.php
@@ -116,7 +116,7 @@ class statibus {
   public function getColor($percentage) {
     if ($percentage == 100) {
       return "green";
-    } elseif ($percentage < 100 && $percentage > 99) {
+    } elseif ($percentage < 99.8 && $percentage > 99) {
        return "darkgreen";
     } elseif ($percentage < 99 && $percentage > 97) {
       return "orange";
